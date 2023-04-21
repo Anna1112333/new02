8ba8d73d576a5e6ad3ef5e475361d535e7c27490
Anna1112333 <anna_1112333@mail.ru>
2023-04-21 21:18:30 +0500

    добавила проектные файлы

---
+++ Task_02_1/main.cpp
@@ -0,0 +1,12 @@
+#include <QCoreApplication>
+
+#include <qtcpsocket.h>
+#include <QSqlDatabase>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+QTcpSocket w;
+QSqlDatabase m;
+    return a.exec();
+}
